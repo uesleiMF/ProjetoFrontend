7f54d134f71f4f8875bf239b8b844c405b43be0a
Jose Ueslei De Moraes Ferreira <60453278+uesleiMF@users.noreply.github.com>
2021-11-30 19:02:33 -0300

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import './App.css';
+import Header from './components/shared/Header/Header';
+import Home from './pages/Home/Home';
+import Cadastro from './pages/Cadastro/Cadastro';
+import { Routes, Route } from 'react-router-dom';
+import View from './pages/View/View';
+import Edit from './pages/Edit/Edit';
+import Inicio from './pages/Inicio/Inicio';
+
+
+function App() {
+  return (
+    
+            <div>   
+        
+      <Header/>
+      
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/cadastro" element={<Cadastro/>}/>
+        <Route path="/view/:id" element={<View/>}/>
+        <Route path="/edit/:id" element={<Edit/>}/>
+        <Route path="/Inicio" element={<Inicio/>}/>
+       
+      </Routes>
+
+      
+      </div>
+
+        
+         
+  );
+}
+
+
+export default App;

7bf0f334e050303e5a02195890076ddd31e8b546
Jose Ueslei De Moraes Ferreira <60453278+uesleiMF@users.noreply.github.com>
2021-11-30 19:01:55 -0300

    Create Home.js

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import ListProdut from '../../components/structure/ListProdut';
+
+const Home = () => {
+  return (
+   
+     
+    <div className="container">
+     <div className="row mb-3">
+    <div class="container bg-info">
+  <div class="nome text-center">
+  <div className="col-9">
+  
+</div>
+<div class="p-1 mb-2 bg-info text-black">
+    
+    <h1 className="text-center h1">CONHEÇA NOSSOS PRODUTOS</h1>
+    <ListProdut/>
+     
+
+      </div>
+     </div>
+    </div>
+   </div>
+  </div>
+  )
+}
+
+
+
+
+export default Home

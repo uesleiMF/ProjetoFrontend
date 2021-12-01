1ea9834dd0c98e429e0bf3757ae4d5fd154690ed
Jose Ueslei De Moraes Ferreira <60453278+uesleiMF@users.noreply.github.com>
2021-12-01 18:48:29 -0300

    Update Header.js

--- src/components/shared/Header/Header.js
+++ src/components/shared/Header/Header.js
@@ -5,43 +5,47 @@ import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
-    
-<Navbar bg="light" expand="lg">
-  <Container fluid>
-    <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
-    <Navbar.Toggle aria-controls="navbarScroll" />
-    <Navbar.Collapse id="navbarScroll">
-      <Nav
-        className="me-auto my-2 my-lg-0"
-        style={{ maxHeight: '100px' }}
-        navbarScroll
-      >
-        <Nav.Link href="/Inicio">Pagina Inicial</Nav.Link>
-        <Nav.Link href="/">Produtos Cadastrados</Nav.Link>
-        <Nav.Link href="/cadastro">Cadastro</Nav.Link>
+    <Navbar bg="light" expand="lg">
+    <Container fluid>
+      <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
+      <Navbar.Toggle aria-controls="navbarScroll" />
+      <Navbar.Collapse id="navbarScroll">
         
-        <NavDropdown title="/cadastro" id="navbarScrollingDropdown">
-          <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-          <NavDropdown.Item href="#action4">Another action</NavDropdown.Item>
-          <NavDropdown.Divider />
-         
-        </NavDropdown>
-     
-      </Nav>
-      <Form className="d-flex">
-        <FormControl
-          type="search"
-          placeholder="Search"
-          className="me-2"
-          aria-label="Search"
-        />
+          <Link href="#action1"  className="navbar-brand" to="/Inicio">
+          Pagina Inicial
+         </Link>
+
+          <Nav.Link href="#action2">Link</Nav.Link>
+          <NavDropdown title="Link" id="navbarScrollingDropdown">
+            <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+            <NavDropdown.Item href="#action4">Another action</NavDropdown.Item>
+            <NavDropdown.Divider />
+            <NavDropdown.Item href="#action5">
+              Something else here
+            </NavDropdown.Item>
+          </NavDropdown>
+          <Nav.Link href="#" disabled>
+            Link
+          </Nav.Link>
+        
+        <Form className="d-flex">
+          <FormControl
+            type="search"
+            placeholder="Search"
+            className="me-2"
+            aria-label="Search"
+          />
+          <Button variant="outline-success">Search</Button>
+        </Form>
+      </Navbar.Collapse>
+    </Container>
+  </Navbar>
+
        
-      </Form>
-    </Navbar.Collapse>
-  </Container>
-</Navbar>
- )
+  
+  )
 }
 
-
 export default Header
+
+
